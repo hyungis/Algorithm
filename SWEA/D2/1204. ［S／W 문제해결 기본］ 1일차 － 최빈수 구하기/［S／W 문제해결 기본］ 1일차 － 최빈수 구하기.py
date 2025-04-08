a15d31744e5b69ae656d5b2f42d937cb4e381143
hyun-gi-man <ghd3466@naver.com>
2025-04-08 20:51:35 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 79 ms, Memory: 54,784 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -1,11 +1,14 @@
-T = int(input())
-for i in range(1, T+1):
-    _ = input()
-    grades = list(map(int, input().split()))
-    freq = [0] * 101 # 점수는 0 ~ 100점까지 있으니까
-    mode = 0 # 최빈값
-    for grade in grades:
-        freq[grade] += 1 # 현재점수의 빈도상승
-        if freq[grade] >= freq[mode]:
-        	mode = grade
-    print(f"#{i} {mode}")
+n = int(input())
+
+for i in range(1, n+1):
+    testcase = int(input())
+    li = list(map(int, input().split()))
+    arr = [0] * 101
+    answer_li = []
+    for j in li:
+        arr[j] += 1
+    
+    for k in range(len(arr)):
+        if arr[k] == max(arr):
+            answer_li.append(k)
+    print(f"#{testcase} {max(answer_li)}")
