de930764d007a481e9c9af361831f58d0abb550e
hyun-gi-man <ghd3466@naver.com>
2025-04-18 21:47:35 +0900

    [level 1] Title: 신고 결과 받기, Time: 1446.42 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,18 +1,11 @@
 def solution(id_list, report, k):
-    answer = []
-    result = []
-    dict = {x:[[],0,0] for x in id_list}
-    for i in set(report):
-        dict[i.split()[0]][0].append(i.split()[1])
-        dict[i.split()[1]][1] += 1
-        
-    for j in id_list:
-        if dict[j][1] >= k:
-            answer.append(j)
-    for i in id_list:
-        for j in answer:
-            if j in dict[i][0]:
-                dict[i][2] += 1
-        result.append(dict[i][2])
+    answer = [0]*len(id_list)
+    dict = {x:0 for x in id_list}
     
-    return result
+    for i in set(report):
+        dict[i.split()[1]] += 1
+    
+    for j in set(report):
+        if dict[j.split()[1]] >= k:
+            answer[id_list.index(j.split()[0])] += 1
+    return answer
