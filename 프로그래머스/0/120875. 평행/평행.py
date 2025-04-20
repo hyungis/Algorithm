f361847c34b9daa17c2d927a08fbed354741b848
hyun-gi-man <ghd3466@naver.com>
2025-04-20 23:58:00 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,9 @@
+def solution(dots):
+    [x1, y1], [x2, y2], [x3, y3], [x4, y4] = dots
+    answer1 = (y2-y1)*(x4-x3) == (y4-y3)*(x2-x1)
+    answer2 = (y3-y1)*(x4-x2) == (y4-y1)*(x4-x2)
+    answer3 = (y4-y1)*(x3-x2) == (y3-y2)*(x4-x1)
+    if answer1 or answer2 or answer3:
+        return 1
+    else:
+        return 0
