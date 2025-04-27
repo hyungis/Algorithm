2f7d12ff3f9d2b82d7bc6049eacac63e01d55dc7
hyun-gi-man <ghd3466@naver.com>
2025-04-27 17:33:06 +0900

    [D2] Title: 파리 퇴치, Time: 64 ms, Memory: 53,760 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -3,15 +3,17 @@ t = int(input())
 for tc in range(1, t+1):
     n, m = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(n)]
+    prefix = [[0]*(n+1) for _ in range(n+1)]
+
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            prefix[i][j] = arr[i-1][j-1] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1]
+
     max_cnt = 0
-    for i in range(n-m+1):
-        for j in range(n-m+1):
-            cnt = 0
 
-            for x in range(i, i+m):
-                for y in range(j, j+m):
-                    cnt += arr[x][y]
-
+    for i in range(m, n+1):
+        for j in range(m, n+1):
+            cnt = prefix[i][j] - prefix[i-m][j] - prefix[i][j-m] + prefix[i-m][j-m]
             if max_cnt < cnt:
                 max_cnt = cnt
     print(f'#{tc} {max_cnt}')
