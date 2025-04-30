39a8c0bb7f76c20d3320216233a8ececd971cc4c
hyun-gi-man <ghd3466@naver.com>
2025-04-30 17:34:41 +0900

    [D2] Title: 중간 평균값 구하기, Time: 56 ms, Memory: 51,072 KB -BaekjoonHub

---
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.py
@@ -0,0 +1,8 @@
+t = int(input())
+
+for tc in range(1, t+1):
+    arr = list(map(int, input().split()))
+    arr.remove(max(arr))
+    arr.remove(min(arr))
+    result = sum(arr)/len(arr)
+    print(f'#{tc} {round(result)}')
