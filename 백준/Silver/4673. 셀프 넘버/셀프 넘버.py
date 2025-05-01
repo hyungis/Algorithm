b8e0602fa3799960f6110b27f2a261a5ee959ae0
hyun-gi-man <ghd3466@naver.com>
2025-05-01 16:53:36 +0900

    [Silver V] Title: 셀프 넘버, Time: 44 ms, Memory: 34464 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,11 +1,9 @@
-num_list = set(range(1, 10001))
+arr = set(range(1,10001))
 x_list = set()
-
-for i in num_list:
+for i in arr:
     for j in str(i):
         i += int(j)
     x_list.add(i)
-
-result = num_list-x_list
+result = arr - x_list
 for i in sorted(result):
     print(i)
