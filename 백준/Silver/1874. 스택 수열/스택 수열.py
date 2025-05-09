9d958125cbe0a32a67b4dfd00082a6951e21d31c
hyun-gi-man <ghd3466@naver.com>
2025-05-09 20:17:00 +0900

    [Silver II] Title: 스택 수열, Time: 2596 ms, Memory: 35020 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,25 @@
+n = int(input())
+stack = []
+result = []
+cnt = 1
+temp =True
+
+for i in range(n):
+    num = int(input())
+
+    while cnt <= num:
+        stack.append(cnt)
+        result.append('+')
+        cnt += 1
+
+    if stack[-1] == num:
+        stack.pop()
+        result.append('-')
+    else:
+        temp = False
+        break
+if not temp:
+    print('NO')
+else:
+    for i in result:
+        print(i)
