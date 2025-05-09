b6a56b4ecff3b1a46e839c984577bbfffe43d930
hyun-gi-man <ghd3466@naver.com>
2025-05-09 23:35:39 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 228 ms, Memory: 49620 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,12 +1,14 @@
-n, m = map(int, input().split())
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
 
 arr = {}
 for _ in range(n):
-    site, pw = input().split()
+    site, pw = sys.stdin.readline().strip().split()
     arr[site] = pw
 result = []
 for i in range(m):
-    x = input()
+    x = sys.stdin.readline().strip()
     result.append(arr[x])
 
 for i in result:
