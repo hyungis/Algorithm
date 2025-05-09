698acdd57e6358ffb897cee1ad96f84c309a315c
hyun-gi-man <ghd3466@naver.com>
2025-05-09 22:21:14 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+
+coins = [int(input()) for _ in range(n)]
+coins.sort(reverse=True)
+
+answer = 0
+for coin in coins:
+    if k >= coin:
+        answer += k // coin
+        k %= coin
+        if k == 0:
+            break
+print(answer)
