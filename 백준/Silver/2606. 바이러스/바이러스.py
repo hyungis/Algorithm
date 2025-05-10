65df93c1eda8f18805c5a18a66d31d75a20d952b
hyun-gi-man <ghd3466@naver.com>
2025-05-10 16:30:05 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -9,20 +9,18 @@ for i in range(m):
     a, b = map(int, sys.stdin.readline().split())
     arr[a].append(b)
     arr[b].append(a)
-
-def bfs():
-    q = deque()
-    q.append(1)
-    visited[1] = True
-    result = 0
-    while q:
-        cur = q.popleft()
-        for val in arr[cur]:
-            if visited[val] == False:
-                q.append(val)
-                visited[val] = True
-                result += 1
-    print(result)
-
 visited = [False for _ in range(n+1)]
-bfs()
+q = deque()
+q.append(1)
+visited[1] = True
+result = 0
+while q:
+    cur = q.popleft()
+    for val in arr[cur]:
+        if visited[val] == False:
+            q.append(val)
+            visited[val] = True
+            result += 1
+
+print(result)
+
