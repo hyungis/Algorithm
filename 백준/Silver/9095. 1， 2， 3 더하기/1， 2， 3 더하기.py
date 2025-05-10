5984a701e86ec23eb961f5e837d04f7fd476ecce
hyun-gi-man <ghd3466@naver.com>
2025-05-10 16:43:30 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,16 @@
+
+def dp(x):
+    if x == 1:
+        return 1
+    elif x == 2:
+        return 2
+    elif x == 3:
+        return 4
+    else:
+        return dp(x-1)+dp(x-2)+dp(x-3)
+
+
+t = int(input())
+for tc in range(t):
+    n = int(input())
+    print(dp(n))
