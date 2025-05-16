9a59eb14bbe645d3b984e32212b52db67e5c1699
hyun-gi-man <ghd3466@naver.com>
2025-05-16 21:05:04 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 73 ms, Memory: 59,392 KB -BaekjoonHub

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -0,0 +1,7 @@
+for tc in range(1, 11):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    for i in range(n):
+        arr[arr.index(max(arr))] -= 1
+        arr[arr.index(min(arr))] += 1
+    print(f'#{tc} {max(arr)-min(arr)}')
