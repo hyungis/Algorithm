487c911d97f48ffe1456de01742956cbead799ad
hyun-gi-man <ghd3466@naver.com>
2025-05-18 17:34:48 +0900

    [Silver I] Title: 오목, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,35 +1,32 @@
-arr = [list(map(int, input().split())) for _ in range(19)]
+import sys
+
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(19)]
 
 dx = [0, 1, 1, -1]
 dy = [1, 0, 1, 1]
-result = 0
+
 for x in range(19):
     for y in range(19):
-        if arr[x][y] != 0:
-            focus = arr[x][y]
+        if board[x][y] != 0:
+            result = board[x][y]
 
             for i in range(4):
                 cnt = 1
                 nx = x + dx[i]
                 ny = y + dy[i]
 
-                while 0 <= nx <= 18 and 0 <= ny <= 18 and arr[nx][ny] == focus:
+                while 0<=nx<19 and 0<=ny<19 and board[nx][ny] == result:
                     cnt += 1
 
                     if cnt == 5:
-                        if 0 <= x - dx[i] <= 18 and 0 <= y - dy[i] <= 18 and arr[x - dx[i]][y - dy[i]] == focus:
+                        if 0<=x-dx[i]<19 and 0<=y-dy[i]<19 and board[x-dx[i]][y-dy[i]] == result:
                             break
-                        if 0 <= nx + dx[i] <= 18 and 0 <= ny + dy[i] <= 18 and arr[nx + dx[i]][ny + dy[i]] == focus:
+                        if 0<=nx+dx[i]<19 and 0<=ny+dy[i]<19 and board[nx+dx[i]][ny+dy[i]] == result:
                             break
-                        if focus == 1:
-                            result = 1
-                        if focus == 2:
-                            result = 2
-                        a, b = x, y
+                        print(result)
+                        print(x+1, y+1)
+                        sys.exit(0)
+
                     nx += dx[i]
                     ny += dy[i]
-if result > 0:
-    print(result)
-    print(a+1, b+1)
-else:
-    print(0)
+print(0)
