76e4578e3b23e83b9e2d24401a5480e20ca93432
hyun-gi-man <ghd3466@naver.com>
2025-07-16 20:05:04 +0900

    [Bronze V] Title: A+B - 5, Time: 104 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringBuilder sb = new StringBuilder();
 		while (true) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			int a = Integer.parseInt(st.nextToken());
@@ -14,8 +14,9 @@ public class Main {
 			if (a == b && a == 0) {
 				break;
 			}
-			System.out.println(a+b);
-
+			sb.append(a+b);
+			sb.append('\n');
 		}
+		System.out.println(sb);
 	}
 }
