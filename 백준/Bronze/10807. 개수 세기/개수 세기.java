3b8e30c570b7e5b7c836352054aff302914d440c
hyun-gi-man <ghd3466@naver.com>
2025-07-16 20:37:09 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,26 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
 		int count = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		for (int i = 0; i<N; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		int b = sc.nextInt();
+		int b = Integer.parseInt(br.readLine());
+		
 		for (int x: arr) {
 			if (x == b) {
 				count += 1;
 			}
 		}
 		System.out.println(count);
-		
-		
-		
 		
 	}
 }
