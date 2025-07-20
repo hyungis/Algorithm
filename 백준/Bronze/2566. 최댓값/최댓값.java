a2dafe6e1dcd9f95607502058e84239f9e0d019a
hyun-gi-man <ghd3466@naver.com>
2025-07-20 13:24:38 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 18512 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] arr = new int[9][9];
+		int max = -1;
+		int x = 0;
+		int y = 0;
+		for(int i = 0; i<9; i++) {
+			for(int j = 0; j<9; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int i = 0; i<9; i++) {
+			for(int j = 0; j<9; j++) {
+				if (arr[i][j] > max) {
+					max = arr[i][j];
+					x = i+1;
+					y = j+1;
+				}
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println(x+" "+y);
+		
+	}
+	
+}
+	
+	
+	
+	
