7e903081d37fa30e774b12379a179fead85bf072
hyun-gi-man <ghd3466@naver.com>
2025-08-07 22:41:38 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1608 ms, Memory: 97240 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	static boolean[] check;
+	static ArrayList<Integer>[] arr;
+	static int count = 0;
+	static int[] result;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		
+		result = new int[n+1];
+		check = new boolean[n+1];
+		arr = new ArrayList[n+1];
+		for(int i = 0; i<arr.length; i++) {
+			arr[i] = new ArrayList<>();
+		}
+		for(int i = 0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			arr[a].add(b);
+			arr[b].add(a);
+		}
+		for(int i = 0; i<arr.length; i++) {
+			Collections.sort(arr[i]);
+		}
+		count++;
+		bfs(r);
+		for(int i = 1; i<=n; i++) {
+			System.out.println(result[i]);
+		}
+		
+	}
+	public static void bfs(int start) {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.add(start);
+		check[start] = true;
+		result[start] = count;
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			for(int v: arr[now]) {
+				if(check[v] == false) {
+					check[v] = true;
+					q.add(v);
+					count++;
+					result[v] = count;
+				}
+			}
+		}
+	}
+
+}
