efa5c670e63b62ceda9c7f430e19abdeec63016e
hyun-gi-man <ghd3466@naver.com>
2025-08-08 23:17:58 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static boolean[] check;
+	static int[] arr;
+	static int n, m;
+	static StringBuilder sb;
+	static int[] arr2;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		
+		check = new boolean[n+1];
+		arr = new int[m];
+		arr2 = new int[n+1];
+		for(int i = 1; i<=n; i++) {
+			arr2[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr2);
+		
+		sb = new StringBuilder();
+		
+		dfs(0, 1);
+		System.out.println(sb);
+		
+		
+	}
+	public static void dfs(int cnt, int start) {
+		if(cnt == m) {
+			for(int v: arr) {
+				sb.append(v).append(" ");
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for(int i = start; i<=n; i++) {
+			arr[cnt] = arr2[i];
+			dfs(cnt+1, i+1);
+		}
+		
+	}
+
+}
