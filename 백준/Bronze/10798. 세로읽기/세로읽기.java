7ded8826727dc83f0712e6812e9a1e2ae39dd28d
hyun-gi-man <ghd3466@naver.com>
2025-08-09 15:48:31 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,35 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char[][] arr = new char[5][15];
-		int i, j;
 		
-		for(i = 0; i < 5; i++) {
-			String str = sc.next();
-			
-			for(j = 0; j < str.length(); j++) {
-				arr[i][j] = str.charAt(j);
+		for(int i = 0; i<5; i++) {
+			String s = br.readLine();
+			for(int j = 0; j<s.length(); j++) {
+				char c = s.charAt(j);
+				arr[i][j] = c;
 			}
 		}
-		
-		for(i=0; i< 15; i++) {
-			for(j=0; j<5; j++) {
-				if (arr[j][i] == '\0') {
-					continue;
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i<15; i++) {
+			for(int j = 0; j<5; j++) {
+				if(arr[j][i] != '\u0000') {
+					System.out.print(arr[j][i]);
 				}
-				System.out.print(arr[j][i]);
+				
 			}
 		}
+		
 		
 		
 	}
-	
+
 }
-	
-	
-	
-	
