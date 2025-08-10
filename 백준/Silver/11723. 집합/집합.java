6e215f79cb1a93f1bc9409f8ffe8d01ca730602a
hyun-gi-man <ghd3466@naver.com>
2025-08-10 16:38:25 +0900

    [Silver V] Title: 집합, Time: 1032 ms, Memory: 313032 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,67 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
-
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		HashSet<Integer> set = new HashSet<>();
-		
-		int n = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			String s = st.nextToken();
-			switch(s) {
-			case "add":{
-				set.add(Integer.parseInt(st.nextToken()));
-				break;
-			}
-			case "remove":{
-				set.remove(Integer.parseInt(st.nextToken()));
-				break;
-			}
-			case "check":{
-				if(set.contains(Integer.parseInt(st.nextToken()))){
-					sb.append(1).append('\n');
-					break;
-				}else {
-					sb.append(0).append('\n');
-					break;
-				}
-			}
-			case "toggle":{
-				int x = Integer.parseInt(st.nextToken());
-				if(set.contains(x)) {
-					set.remove(x);
-					break;
-				}else {
-					set.add(x);
-					break;
-				}
-			}
-			case "all":{
-				for(int j = 1; j<=20; j++) {
-					set.add(j);
-				}
-				break;
-			}
-			case "empty":{
-				set.clear();
-				break;
-			}
-			
-			}
-		}
-		
-		System.out.println(sb);
-		
-		
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int s = 0;
+        int m = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i<m; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	String str = st.nextToken();
+        	switch (str) {
+        	case "add":{
+        		s |= (1<<Integer.parseInt(st.nextToken()));
+        		break;
+        	}
+        	case "remove":{
+        		s &= ~(1<<Integer.parseInt(st.nextToken()));
+        		break;
+        	}
+        	case "check":{
+        		if( (s & (1<<Integer.parseInt(st.nextToken())) ) == 0){
+        			sb.append(0).append('\n');
+        			break;
+        		}else {
+        			sb.append(1).append('\n');
+        			break;
+        		}
+        	}
+        	case "toggle":{
+        		s ^= (1<<Integer.parseInt(st.nextToken()));
+        		break;
+        		}
+        	case "all":{
+        		s = (1<<21)-1;
+        		break;
+        	}
+        	case "empty":{
+        		s = 0;
+        		break;
+        	}
+        	}
+        }
+        System.out.println(sb);
+        
+        
+    }
 }
