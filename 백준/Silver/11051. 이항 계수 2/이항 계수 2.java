f73698f2cac3f85e5215ee34defb81bf4e165e64
hyun-gi-man <ghd3466@naver.com>
2025-09-04 14:26:21 +0900

    [Silver II] Title: 이항 계수 2, Time: 120 ms, Memory: 17984 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[][] dp = new int[n+1][n+1];
+		
+		for(int i = 0; i<=n; i++) {
+			for(int j = 0; j<=i; j++) {
+				if(j == 0 || i == j) dp[i][j] = 1;
+				else {
+					dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%10007;
+				}
+			}
+		}
+		
+		System.out.println(dp[n][k]);
+		
+	}
+
+
+}
