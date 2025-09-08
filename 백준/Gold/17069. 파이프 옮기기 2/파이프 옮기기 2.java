b4ac4fda7fa4de16ca617784baf361ca91ee72cd
hyun-gi-man <ghd3466@naver.com>
2025-09-08 14:32:28 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 112 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -21,26 +21,22 @@ public class Main {
         }
 
         dp[0][1][0] = 1;  // 최초 상태
-        for(int i = 0; i < n; i++) {
-            for(int j = 2; j < n; j++) {
-                if(j - 1 >= 0 && map[i][j] == 0) {  // 가로
-                    dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
-                }
-                if(i - 1 >= 0 && map[i][j] == 0) {  // 세로
-                    dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
-                }
-                if(i-1 >= 0 && j-1 >= 0 && map[i][j] == 0 && map[i-1][j] == 0 && map[i][j-1] == 0) {  // 대각
-                    dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
-                }
-            }
+        for(int i = 0; i<n; i++) {
+        	for(int j = 2; j<n; j++) {
+        		if(j-1 >= 0 && map[i][j] == 0) {
+        			dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
+        		}
+        		if(i-1 >= 0 && map[i][j] == 0) {
+        			dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+        		}
+        		if(i-1 >= 0 && j-1 >= 0 && map[i-1][j] == 0 && map[i][j-1] == 0 && map[i][j] == 0) {
+        			dp[i][j][2] = dp[i-1][j-1][2] + dp[i-1][j-1][1] + dp[i-1][j-1][0];
+        		}
+        	}
         }
-
-        long ans = dp[n-1][n-1][0] + dp[n-1][n-1][1] + dp[n-1][n-1][2];
-        System.out.println(ans);
 		
-		
-		
-		
+		long result = dp[n-1][n-1][0] + dp[n-1][n-1][1] + dp[n-1][n-1][2];
+		System.out.println(result);
 		
 		
 	}
