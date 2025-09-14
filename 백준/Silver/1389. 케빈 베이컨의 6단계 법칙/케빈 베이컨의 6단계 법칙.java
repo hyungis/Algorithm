700ad554424c95a9b99c938900fffdfdb4ec61af
hyun-gi-man <ghd3466@naver.com>
2025-09-14 15:57:03 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 128 ms, Memory: 14648 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -3,62 +3,56 @@ import java.util.*;
 
 public class Main {
 
-	static int n, m;
-	static ArrayList<Integer>[] arr;
-	static boolean[] visited;
-	static int ans = -1;
-	static int min = Integer.MAX_VALUE;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		
 		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		arr = new ArrayList[n+1];
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[][] result = new int[n+1][n+1];
 		
 		for(int i = 1; i<=n; i++) {
-			arr[i] = new ArrayList<>();
+			for(int j = 1; j<=n; j++) {
+				result[i][j] = 987654321;
+				if( i == j ) {
+					result[i][j] = 0;
+				}
+			}
 		}
 		
 		for(int i = 0; i<m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			arr[a].add(b);
-			arr[b].add(a);
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			result[x][y] = result[y][x] = 1;
 		}
 		
-
+		for(int k = 1; k<=n; k++) {
+			for(int i = 1; i<=n; i++) {
+				for(int j = 1; j<=n; j++) {
+					result[i][j] = Math.min(result[i][j], result[i][k] + result[k][j]);
+				}
+			}
+		}
+		int res = Integer.MAX_VALUE;
+		int ans = -1;
 		for(int i = 1; i<=n; i++) {
-			visited = new boolean[n+1];
-			bfs(i, 0);
+			int total = 0;
+			for(int j = 1; j<=n; j++) {
+				total += result[i][j];
+			}
+			if(total < res) {
+				res = total;
+				ans = i;
+			}
 		}
 		System.out.println(ans);
 		
 	}
 	
-	static void bfs(int start, int cnt) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {start, cnt});
-		visited[start] = true;
-		
-		int result = 0;
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			
-			for(int v : arr[now[0]]) {
-				if(visited[v]) continue;
-				q.offer(new int[] {v, now[1]+1});
-				result += now[1]+1;
-				visited[v] = true;
-			}
-		}
-		if(result < min) {
-			min = result;
-			ans = start;
-		}
-	}
 
 }
