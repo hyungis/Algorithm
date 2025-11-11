df8c9455fa531351988245a1243b9ce94cb4d0b3
hyun-gi-man <ghd3466@naver.com>
2025-11-12 00:27:53 +0900

    [Silver II] Title: 에디터, Time: 356 ms, Memory: 55172 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,54 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.ListIterator;
-import java.util.StringTokenizer;
-
 
 public class Main {
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static void main(String[] args) throws IOException {
-		String s = br.readLine();
-		LinkedList<Character> list = new LinkedList<>();
-		for(int i = 0; i<s.length(); i++) {
-			list.add(s.charAt(i));
-		}
-		
-		ListIterator<Character> iter = list.listIterator(list.size());
-		int m = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i<m; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String a = st.nextToken();
-			if(a.equals("P")) {
-				iter.add(st.nextToken().charAt(0));
-			}else if(a.equals("L")) {
-				if(iter.hasPrevious()) {
-					iter.previous();
-				}
-			}else if(a.equals("D")) {
-				if(iter.hasNext()) {
-					iter.next();
-				}
-			}else if(a.equals("B")) {
-				if(iter.hasPrevious()) {
-					iter.previous();
-					iter.remove();
-				}
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		for(char a: list) {
-			sb.append(a);
-		}
-		System.out.println(sb);
-		
-		
-		
-		
-	
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        int M = Integer.parseInt(br.readLine());
+        
+        LinkedList<Character> list = new LinkedList<Character>();
+
+        for(int i = 0; i < str.length(); i++) {
+            list.add(str.charAt(i));
+        } //입력받은 문자열을 배열이 아닌 linkelist 형태로 저장
+
+        //iterator 메소드 호출
+        ListIterator<Character> iter = list.listIterator();
+        //선언 시에는 리스트의 맨 앞에  위치( | a b c )
+
+        //처음 커서는 문장의 맨 뒤에 있어야하기 때문에 커서를 맨뒤로 이동시켜줌 (ex. abc|)
+        while(iter.hasNext()) {
+            iter.next(); //( a | b c )
+        }
+
+        for(int i = 0; i < M; i++) {
+            String command = br.readLine();
+            char c = command.charAt(0);
+            switch(c) {
+                case 'L':
+                    if(iter.hasPrevious())
+                        iter.previous();
+
+                    break;
+                case 'D':
+                    if(iter.hasNext())
+                        iter.next();
+
+                    break;
+                case 'B':
+                    //remove()는 next()나 previous()에 의해 반환된 가장 마지막 요소를 리스트에서 제거함
+                    if(iter.hasPrevious()) {
+                        iter.previous();
+                        iter.remove();
+                    }
+                    break;
+                case 'P':
+                    char t = command.charAt(2);
+                    iter.add(t);
+                    //현재 커서의 위치 바로 뒤에 요소가 추가
+                    break;
+                default:
+                    break;
+            }
+        }
+        for(Character chr : list) {
+            bw.write(chr);
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }
