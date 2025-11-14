cb39229be609de6f7e31fb3c82f785adedcd88a9
hyun-gi-man <ghd3466@naver.com>
2025-11-14 21:52:38 +0900

    [Silver II] Title: 쇠막대기, Time: 148 ms, Memory: 16416 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,30 +1,49 @@
-import java.util.Stack;
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		Stack<Character> stack = new Stack<>();
-		int result = 0;
-		for(int i = 0; i<s.length(); i++) {
-			char c = s.charAt(i);
-			if(c == '(') {
-				stack.push(c);
-			} else if (c == ')') {
-				if (s.charAt(i-1) == '(') {
+    
+	
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	StringBuilder sb = new StringBuilder();
+    	
+    	String s = br.readLine();
+    	Stack<Character> stack = new Stack<>();
+    	
+    	int result = 0;
+    	
+    	for(int i = 0; i<s.length(); i++) {
+    		char c = s.charAt(i);
+    		
+    		if(c == '(') {
+    			stack.add(c);
+    		}
+    		else {
+    			if(s.charAt(i-1) == '(') {
+    				stack.pop();
+    				result += stack.size();
+    			}
+    			else {
+    				result++;
 					stack.pop();
-					result += stack.size();
-				} else {
-					result += 1;
-					stack.pop();
-				}
-			}
-		}
-		System.out.println(result);
-		
-	}
-
+    				
+    			}
+    		}
+    	}
+    	System.out.println(result);
+    		
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
