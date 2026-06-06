9a925e33f2d0f818a77ae85c55b9329828ce9d01
HyunGi Hong <ghd3466@naver.com>
2026-06-06 18:25:24 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.26 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/README.md
+++ 프로그래머스/1/12931. 자릿수 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자릿수 더하기 - 12931 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 0.05 ms
+메모리: 64.7 MB, 시간: 0.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 18일 00:09:13
+2026년 06월 06일 18:25:22
 
 ### 문제 설명
 
--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,10 +2,13 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        String s = String.valueOf(n);
         int answer = 0;
-        for (int i = 0; i < s.length(); i++){
-            answer += Integer.parseInt(String.valueOf(s.charAt(i)));
+
+        String s = String.valueOf(n);
+        for(int i = 0; i<s.length(); i++){
+            int c = s.charAt(i) - '0';
+            answer += c;
+            System.out.println(answer);
         }
 
         return answer;
