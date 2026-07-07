4b3f11fe308e3735290b3ef265a8d375c7dde11a
HyunGi Hong <ghd3466@naver.com>
2026-07-07 13:19:45 +0900

    [level 2] Title: 소수 찾기, Time: 20.00 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    static boolean[] visited = new boolean[7];
+    static HashSet<Integer> set = new HashSet<>();
+    
+    public int solution(String numbers) {
+        int answer = 0;
+        for(int i = 0; i<numbers.length(); i++){
+            dfs(numbers, "", i+1);
+        }
+        System.out.println(set.size());
+        return set.size();
+    }
+    
+    public void dfs(String str, String temp, int m){
+        if(temp.length() == m){
+            int num = Integer.parseInt(temp);
+            if(isPrime(num)){
+                set.add(num);
+            }
+        }
+        
+        for(int i = 0; i<str.length(); i++){
+            if(!visited[i]){
+                visited[i] = true;
+                temp += str.charAt(i);
+                dfs(str, temp, m);
+                visited[i] = false;
+                temp = temp.substring(0, temp.length() -1);
+            }
+        }
+    }
+    
+    public boolean isPrime(int n){
+        if(n < 2){
+            return false;
+        }    
+        
+        for(int i = 2; i*i <= n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
