58e57fc2f992ec9fb78ad5268c69603dcc6c15ee
HyunGi Hong <ghd3466@naver.com>
2026-08-05 13:34:20 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 11.32 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Deque<Integer> q = new ArrayDeque<>();
+        
+        int sum = 0;
+        int time = 0;
+        for(int i = 0; i<truck_weights.length; i++){
+            int truck = truck_weights[i];
+            
+            while(true){
+                if(q.isEmpty()){
+                    q.add(truck);
+                    sum += truck;
+                    time++;
+                    break;
+                }else if(q.size() == bridge_length){
+                    sum -= q.poll();
+                }else{
+                    if(sum + truck <= weight){
+                        q.add(truck);
+                        sum += truck;
+                        time++;
+                        break;
+                    }else{
+                        q.add(0);
+                        time++;
+                    }
+                }
+            }
+        }
+        
+        return time+bridge_length;
+    }
+}
